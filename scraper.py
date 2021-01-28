372927ab76a0dd2ae3fc68be395a809b52b6cc50
Natcha Jengjirapas <njengjir@uci.edu>
2021-01-28 13:50:11 -0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,12 +1,10 @@
 import re
-from urllib.parse import urlparse, urldefrag, urlsplit, urlunsplit
+from urllib.parse import urlparse, urldefrag, urlsplit, urlunsplit, urljoin
 from bs4 import BeautifulSoup
 import requests
 from requests.exceptions import HTTPError
 
 VALID_URLS = {'ics.uci.edu', '.cs.uci.edu', '.informatics.uci.edu', '.stat.uci.edu', 'today.uci.edu'}
-INVALID_PATH = {'pdf', 'ppt', 'css', 'js'}
-CRAWLED = set()
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -14,37 +12,49 @@ def scraper(url, resp):
 
 def extract_next_links(url, resp):
     next_link = []
-    parsed = urlparse(url)
-    # t = urlunsplit(urlsplit(url)._replace(query="", fragment=""))
+    try:
 
-    if 200 <= resp.status <= 599:
+        # Check if url status is withing 200 - 202 
+        # -------------------------------------------------------------------------------------------------------
+        # 200 = OK - request has succeeded
+        # 201 = Created - request has been fulfilled and resulted in a new resource being created
+        # 202 = Accepted - request has been accepted for processing, but the processing has not been completed
+        # -------------------------------------------------------------------------------------------------------
+        if 200 <= resp.status <= 202 and check_traps(url):
+            parsed = urlparse(url)
+            host = "https://" + parsed.netloc
 
-        soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
-        for link in soup.find_all('a'):
-            l = link.get('href')
-            t = urlunsplit(urlsplit(l)._replace(query="", fragment=""))
-            if l != '#' and is_valid(l) and t not in CRAWLED:
-                # print(t, t not in CRAWLED)
-                CRAWLED.add(t)
-                # print(l)
+            soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+            for link in soup.findAll('a'):
+                try:
+                    # constructing absolute URLs, avoiding duplicate contents 
+                    l = urljoin(host, link['href'])
+                    next_link.append(urldefrag(l)[0])
+        
+                # href attribute does not exist in <a> tag.
+                except KeyError:
+                    print("Status Code:", resp.status, "\nError Message: href attribute does not exist.")
+    
+    # Checks for restricted page.
+    except AttributeError:
+        print("Status Code:", resp.status, "\nError Message:", resp.error)
 
-                # Check for web error message : for debug purposes 
-                # try:
-                #     response = requests.get(l)
-
-                #     # If the response was successful, no Exception will be raised
-                #     response.raise_for_status()
-                # except HTTPError as http_err:
-                #     print(f'HTTP error occurred: {http_err}')  # Python 3.6
-                # except Exception as err:
-                #     print(f'Other error occurred: {err}')  # Python 3.6
-                #     print(url)
-                # else:
-                #     print('Success!')
-
-                # Only append unique url : strip url fragment 
-                next_link.append(urldefrag(l)[0])
     return next_link
+
+def check_traps(url):
+    parsed = urlparse(url)
+
+    # Blacklist
+    if parsed.netloc == "today.uci.edu":
+        return False
+
+    if parsed.netloc == "wics.ics.uci.edu":
+        return False
+    
+    if parsed.netloc == "evoke.ics.uci.edu":
+        return False
+
+    return True
 
 def is_valid(url):
     try:
@@ -66,17 +76,17 @@ def is_valid(url):
         if valid_domain == False:
             return False
 
-        # Check Path : filter out urls that don't point to webpage 
-        for path in INVALID_PATH:
-            if path in parsed.path:
-                return False
+        # # Check Path : filter out urls that don't point to webpage 
+        # for path in INVALID_PATH:
+        #     if path in parsed.path:
+        #         return False
             # print(parsed.path.lower())
 
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+            + r"|ps|eps|tex|ppt|pptx|ppsx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
